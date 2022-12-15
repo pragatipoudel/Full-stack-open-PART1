f7faf1a3c5d0317c0b06382bed81ee5ea4280b91
Pragati Poudel <pragati.poudel37@gmail.com>
2022-12-15 16:42:55 -0500

    Add Part1-4

--- part1-exercise-1-1/src/App.js
+++ part1-exercise-1-1/src/App.js
@@ -32,26 +32,26 @@ const Total = (props) => {
 
 const App = () => {
   const course ='Half Stack application development'
-  const part1 = {
+  const parts = [{
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name:'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
-  }
+  }]
 
 
   return (
 
     <div>
       <Header name={course} />
-      <Content part1 = {part1.name} exercise1 = {part1.exercises} part2 = {part2.name} exercise2 = {part2.exercises} part3 = {part3.name} exercise3 = {part3.exercises}/>
-      <Total exercise1 = {part1.exercises} exercise2 = {part2.exercises} exercise3 = {part3.exercises} />
+      <Content part1 = {parts[0].name} exercise1 = {parts[0].exercises} part2 = {parts[1].name} exercise2 = {parts[1].exercises} part3 = {parts[2].name} exercise3 = {parts[2].exercises}/>
+      <Total exercise1 = {parts[0].exercises} exercise2 = {parts[1].exercises} exercise3 = {parts[2].exercises} />
     </div>
 )}
 
