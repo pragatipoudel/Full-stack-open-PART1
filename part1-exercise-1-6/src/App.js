34c37685feb041e4e79f04fe8988e5dff1875aa4
Pragati Poudel <pragati.poudel37@gmail.com>
2022-12-19 16:47:09 -0500

    Add Part 1-10

--- part1-exercise-1-6/src/App.js
+++ part1-exercise-1-6/src/App.js
@@ -4,6 +4,10 @@ const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 )
 
+const StatisticLine = ({text, value}) => (
+  <p>{text} {value}</p>
+)
+
 const Statistics = ({good, neutral, bad}) => {
   const all = good + bad + neutral
   const average = (good - bad)/all
@@ -16,12 +20,12 @@ const Statistics = ({good, neutral, bad}) => {
   
   return (
     <>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p> average {average}</p>
-      <p>positive {positive} %</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive} />
     </>
   )
 
